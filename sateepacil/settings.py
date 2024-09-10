9259a24251e7e4c88a9a5cc6d49926cff72670ff
ruzbehan <baqliruzbehan@gmail.com>
2024-09-11 00:30:55 +0700

    commit ke 3

--- sateepacil/settings.py
+++ sateepacil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&-6&w376&9^$bw%gs5-=wb0j*o(zb$l3ea23%0r9otljpzthqs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","muhammad-ruzbehan-belisate.pbp.cs.ui.ac.id"]
 
 # Application definition
 
